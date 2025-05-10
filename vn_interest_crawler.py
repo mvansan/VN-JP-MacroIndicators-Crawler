1f7a84d1a506ca489c9146139075ba06871f91ad
mvansan <anmac2206@gmail.com>
2025-05-10 09:31:20 +0700

    vn_interest_data: up to date 31/12/2022

--- vn_interest_crawler.py
+++ vn_interest_crawler.py
@@ -19,8 +19,8 @@ time.sleep(3)
 csv_file = "lai_suat_qua_dem.csv"
 header_written = os.path.exists(csv_file) and os.path.getsize(csv_file) > 0
 
-start_date = datetime.date(2021, 1, 1)
-end_date = datetime.date(2021, 12, 31)
+start_date = datetime.date(2022, 4, 1)
+end_date = datetime.date(2022, 12, 31)
 current_date = start_date
 
 while current_date <= end_date:
